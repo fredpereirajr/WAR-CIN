d533d7d8050c9d4549a1858847684274af510667
fredpereirajr <cfpj@cin.ufpe.br>
2022-04-27 12:45:41 -0300

    distribuicao inicial dos paises

---
+++ Country.js
@@ -0,0 +1,13 @@
+class Country {
+
+    constructor(nome, tropas, fronteira) {
+        this.nome = nome;
+        this.tropas = tropas;
+        this.fronteira = fronteira;
+    }
+
+
+
+}
+
+export default Country;
---
+++ Jogador.js
@@ -0,0 +1,16 @@
+class Jogador {
+
+    constructor(nome, cor) {
+        this.nome = nome;
+        this.cor = cor;  //cor é um objeto que receber o valor hexadecimal da cor e um index
+        this.tropasRodada = 0;
+        this.tropasTotal = 0;
+        this.territorios = [];
+        this.cartas = [];
+    }
+
+
+
+}
+
+export default Jogador;
